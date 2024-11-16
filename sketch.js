bbc812bea0b497f629da094f29606b85e886dd95
quackmyheart <85214314+quackmyheart@users.noreply.github.com>
2024-11-16 18:10:04 -0500

    trying to create sattelite

--- sketch.js
+++ sketch.js
@@ -1,10 +1,25 @@
 let debris_on_screen = [];
 let debris_type = ["metal", "plastic", "glass", "coolant"]; 
-
+let debris_colour = [[128, 128, 128], [255, 192, 203], [0, 0, 255], [255, 255, 255]]; 
 
 function setup() {
   createCanvas(800, 600);
 }
+
+
+class Sat {
+  constructor() {
+    this.num = 0; 
+  }
+  
+  display(){
+    fill(color(255, 0, 255));
+    square(30, 20, 55, 20)
+  }
+}
+satellite = new Sat(); 
+
+
 
 function draw() {
   background(0);
@@ -18,15 +33,21 @@ function draw() {
   for (let debris of debris_on_screen) {
     debris.display();
   }
+  
+  satellite.display(); 
+  
 }
+
+
 
 class Debris {
   constructor(x, y) {
     // this.pos = createVector(x, y);
+    this.num = Math.floor(Math.random() * debris_type.length); 
     this.x = x; 
     this.y = y; 
-    this.material = debris_type[Math.floor(Math.random() * debris_type.length)]; 
-    this.color_for_now = ;
+    this.material = debris_type[this.num]; 
+    this.color_for_now = debris_colour[this.num];
     // this.size = 
     // this.position = 
   }
@@ -36,7 +57,7 @@ class Debris {
   }
   
   display() {
-    fill(color(100,100, 0));
+    fill(color(this.color_for_now[0], this.color_for_now[1], this.color_for_now[2]));
     noStroke();
     ellipse(this.x, this.y, 20);
   }
